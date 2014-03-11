0463884d853de2a3d8acc9d798a6b9136db36276
Tingpeng Huang <robin_1966@163.com>
2014-03-11 14:15:39 +0800

    add some annotations in post.rb [Tingpeng]

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title, :tags_attributes
+  #继承的Active Record 讓你的 Model 有非常多功能，包括基本的資料庫CRUD操作，
+  #資料驗證(data validation)、厲害的搜尋以及可以和其他 Models 關聯在一起。
+  #Rails 提供各種驗證的方法，包括必填、唯一性、格式或是需要有關聯物件。
   validates :name,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
